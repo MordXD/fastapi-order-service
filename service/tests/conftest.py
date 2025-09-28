557ffe7647530a7420adcc9b2e6dda1a5faa448e
MordXD <kotkinegor78@gmail.com>
2025-09-28 19:35:44 +0300

    fix: test autodelete db

--- service/tests/conftest.py
+++ service/tests/conftest.py
@@ -28,12 +28,12 @@ app.dependency_overrides[get_db_connection] = override_get_db_connection
 @pytest.fixture(scope="session", autouse=True)
 def cleanup_database():
     yield
-    print("\nCleaning up test data...")
-    conn = connection_pool.getconn()
-    with conn.cursor() as cursor:
-        cursor.execute("TRUNCATE clients, categories, products, orders, order_items, inventory RESTART IDENTITY CASCADE;")
-    conn.commit()
-    connection_pool.putconn(conn)
+    # print("\nCleaning up test data...")
+    # conn = connection_pool.getconn()
+    # with conn.cursor() as cursor:
+    #     cursor.execute("TRUNCATE clients, categories, products, orders, order_items, inventory RESTART IDENTITY CASCADE;")
+    # conn.commit()
+    # connection_pool.putconn(conn)
 
 @pytest.fixture(scope="module")
 def test_client():
